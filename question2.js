cfcec1a94d5748c004758abca6927a3a40f4ff5a
9307610754 <65156304+9307610754@users.noreply.github.com>
2020-08-16 16:20:10 +0530

    Add files via upload

---
+++ question2.js
@@ -0,0 +1,26 @@
+function submit() {
+    let a = document.querySelector("#username").value;
+    console.log(a);
+    let b = document.querySelector("#password").value;
+    console.log(b);
+
+
+    document.querySelector("#read").value;
+
+
+
+
+
+    if (a = "") {
+        document.querySelector("#username").value = "";
+
+    }
+    else {
+        document.querySelector("#username").value;
+    }
+}
+
+
+
+
+
